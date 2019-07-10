6ed32cff092ef1a7ba08626fee13037dd990eda9
Jessie James <jess.jamestwoo11@gmail.com>
2019-07-10 11:59:40 -0600

    helloClass,helloJokes,daily-planet-deditor

---
+++ exercise/helloClass/helloClass.js
@@ -0,0 +1,19 @@
+//Task 1
+var myfunction = function(pram1, pram2) {
+  return pram1 + pram2;
+  console.log("Hello Class");
+};
+console.log(myfunction);
+// Task 2
+function declaredFunction(name) {
+  return name;
+}
+console.log(declaredFunction("hello VIETNAM"));
+
+// Task 3
+
+function declaredFunction(name, age) {
+  return name + age;
+}
+
+console.log(declaredFunction("Hello Addie", "you are almost 1!"));
