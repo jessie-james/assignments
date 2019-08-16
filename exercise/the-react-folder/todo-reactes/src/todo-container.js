7d943b0d83ccdcd974c3a2073a3d61a64172c6cd
Jessie James <jess.jamestwoo11@gmail.com>
2019-08-16 17:42:32 -0600

    todo lists

--- exercise/the-react-folder/todo-reactes/src/todo-container.js
+++ exercise/the-react-folder/todo-reactes/src/todo-container.js
@@ -2,12 +2,26 @@ import React from 'react'
 import axios from 'axios'
 import Todo from './todo.js'
 
-const TodoContainer = () => {
-    return (
-        <div>
-            <h2>its jess</h2>
-            <Todo {todo.name}/>
-    </div>
-    )
+class TodoContainer extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+            todoArr: [],
+
+        }
+    }
+    componentDidMount() {
+        axios.get(`https://api.vschool.io/jessie_mae/todo/`).then(response => {
+            this.setState({todoArr:response.data})
+        })
+    }
+    render() {
+        const mappedtodoArr = this.state.todoArr.map((todo, i) => <Todo key={i + todo.tile} todo={todo} />)
+        return (
+            <div className="todo-continer">
+                {mappedtodoArr}
+             </div>                
+        )}
 }
+
 export default TodoContainer
--- exercise/the-react-folder/todo-reactes/src/todo.js
+++ exercise/the-react-folder/todo-reactes/src/todo.js
@@ -1,10 +1,33 @@
 import React from 'react'
-const Todo = (props) => {
-    let {title, desciption, price , image } = props.todo
-    return (
-        <div>my list
-            {title}
-        </div>
-    )
+import axios from 'axios';
+class Todo extends React.Component{
+    constructor(props) {
+        super()
+        let { title, desciption, price, image, _id} = props.todoArr
+        this.state = {
+            title: title,
+            description: desciption,
+            price: price,
+            imgUrl: image 
+        }
+    }
+    // handleChange = (e) => {
+    //     let { name, value } = e.target
+    //     this.setState({ [name]: value })
+    // }
+    // handleSubmit = (e) => {
+    //     e.preventDefault()
+    //     axios.put(`https://api.vschool.io/jessie_mae/todo/${this.props.todo_id}`, editedTodo)
+    // }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.title}</h1>
+                <p>{this.state.desciption}</p>
+                <h4>{this.state.price}</h4>
+            </div>)
+       
+   }
+    
 }
 export default Todo
