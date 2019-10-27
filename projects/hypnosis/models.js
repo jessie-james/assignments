b8fe4fd8aab2b63b0e9392fc7507964a748b76ab
Jessie James <jess.jamestwoo11@gmail.com>
2019-10-27 10:51:55 -0600

    model build for the user

---
+++ projects/hypnosis/models.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    appointments: {
+        firstAppointment: {
+            date:Number,
+            time: Number,
+            program:String,
+        },
+        SecondeAppointment: {
+            date: Number,
+            time: Number,
+            program: String,
+        },
+        ThirdAppointment: {
+            date: Number,
+            time: Number,
+            program: String,
+        },
+    }
+});
