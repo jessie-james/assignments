ae7145ca61dd51156ac25f60b1ec7b02b5c767ed
Jessie James <jess.jamestwoo11@gmail.com>
2019-10-31 09:45:07 -0600

    interview question

--- exercise/jspractice/functions.js
+++ exercise/jspractice/functions.js
@@ -1,30 +1,39 @@
-function sum(num1, num2) {
-    return num1 + num2
-}
+// function sum(num1, num2) {
+//     return num1 + num2
+// }
 
-function largest(num1, num2, num3) {
-    return Math.max(num1, num2, num3)
-}
+// function largest(num1, num2, num3) {
+//     return Math.max(num1, num2, num3)
+// }
+Array.prototype.mapV2 = function (callBack) {
+    const newArray =[]
+    for (i = 0; i <= this.length; i++){
+        const newNum = callback(this[i])
+        newArray.push(newNum)
 
 
-function evenOdd(num1) {
-    if (num1 % 2 === 0) {
-        return "even"
-    } else {
-        return "odd"
     }
+    return newArray
 }
 
-function strlength(str) {
-    if (str.length <= 20) {
-        return `${str} ${str}`
-    } else {
-        return str.substr(0, (str.length / 2))
-    }
-}
+// function evenOdd(num1) {
+//     if (num1 % 2 === 0) {
+//         return "even"
+//     } else {
+//         return "odd"
+//     }
+// }
 
-function printFirst(n) {
+// function strlength(str) {
+//     if (str.length <= 20) {
+//         return `${str} ${str}`
+//     } else {
+//         return str.substr(0, (str.length / 2))
+//     }
+// }
+
+// function printFirst(n) {
     
-}
+// }
 
 
