81d6ccaf00b9a3ca9fcaf0aa61dd1ffed6faacbe
Jessie James <jess.jamestwoo11@gmail.com>
2019-11-01 20:32:29 -0600

    quick build from memmoey

---
+++ exercise/interview-practice/app.js
@@ -0,0 +1,5 @@
+let test = document.getElementById("test");
+test.addEventListener("mouseover", function () {
+    test.style.background = "blue";
+    console.log("blue");
+})
---
+++ exercise/interview-practice/interview-practice-reactbuild/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"
+
+export default function App() {
+    return (
+        <div className="contatiner">
+            <h1 className="greeting">hi</h1>
+            <h1>my name</h1>
+            <h1> is Jessie</h1>
+        </div>
+    )
+}
