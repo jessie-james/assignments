47d91099eaa063fb0e7ef20ae56ea9a4325577fc
Jessie James <jess.jamestwoo11@gmail.com>
2019-11-04 11:21:16 -0700

    pop up

---
+++ projects/hypnosis/client/src/LandingPage/Popup.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './popupstyle.css';
+
+class Popup extends React.Component {
+    render() {
+        return (
+            <div className='popup'>
+                <div className='popup\_inner'>
+                    <h1>{this.props.text}</h1>
+                    <button onClick={this.props.closePopup}>close me</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Popup;
--- projects/hypnosis/client/src/LandingPage/index.js
+++ projects/hypnosis/client/src/LandingPage/index.js
@@ -1,16 +1,36 @@
-import React from "react"
+import React ,{Component} from "react"
+import Popup from "./Popup"
 import { Link } from "react-router-dom"
 
-function LandingPage() {
-    return (
-        <div className="homeContainer">
-            <div>logo</div>
-            <div>
-                carrasol
+class LandingPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { showPopup: false };
+    }
+
+    togglePopup() {
+        this.setState({
+            showPopup: !this.state.showPopup
+        });
+    }
+
+    render() {
+        return (
+            <div className="homeContainer">
+                <div>logo</div>
+                <button onClick={this.togglePopup.bind(this)}> Click To Launch Popup</button>
+                {this.state.showPopup ?
+                    <Popup text='Click "Close Button" to hide popup' closePopup={this.togglePopup.bind(this)} />
+                    : null
+                }
+                <div>
+                    carrasol
             </div>
-            <div>Call Me</div>
-            <Link to="/book">Book Now</Link>
-        </div>
-    )
+                <div>Call Me</div>
+                <Link to="/book">Book Now</Link>
+            </div>
+        )
+    }
 }
 export default LandingPage;
